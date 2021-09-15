2d5fbebc9cb12f06b6ad9ad20d76c5cda362668b
Gleb Markin <gleb.markin@banuba.com>
2021-09-15 16:37:22 +0300

    0.0.19

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.3
+import PackageDescription
+let package = Package(
+  name: "BanubaARCloudSDK",
+  platforms: [
+    .iOS(.v11)
+  ],
+  products: [
+    .library(
+      name: "BanubaARCloudSDK",
+      targets: ["BanubaARCloudSDK"])
+  ],
+  targets: [
+    .binaryTarget(
+      name: "BanubaARCloudSDK",
+      path: "BanubaARCloudSDK.xcframework")
+  ])
