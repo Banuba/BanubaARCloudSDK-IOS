a858b5da13f6609de1cf2084591341293ef10012
Andrey Sak <andrei.sak@banuba.com>
2022-11-28 18:44:40 +0300

    automatic framework update

--- BanubaARCloudSDK.xcframework/ios-arm64_x86_64-simulator/BanubaARCloudSDK.framework/Headers/BanubaARCloudSDK.h
+++ BanubaARCloudSDK.xcframework/ios-arm64_x86_64-simulator/BanubaARCloudSDK.framework/Headers/BanubaARCloudSDK.h
@@ -12,7 +12,3 @@ FOUNDATION_EXPORT double BanubaARCloudSDKVersionNumber;
 
 //! Project version string for BanubaARCloudSDK.
 FOUNDATION_EXPORT const unsigned char BanubaARCloudSDKVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <BanubaARCloudSDK/PublicHeader.h>
-#import <BanubaARCloudSDK/SSZipCommon.h>
-#import <BanubaARCloudSDK/SSZipArchive.h>
