fce105a3c4a17a26099ddea263140d13cb108782
CI <ci@banuba.com>
2025-01-23 14:12:37 +0300

    automatic framework update

--- Package.swift
+++ Package.swift
@@ -2,16 +2,21 @@
 
 import PackageDescription
 
+let utilsVersionRange: Range<Version> = "1.40.0"..<"1.49.99"
+
 let package = Package(
   name: "BanubaARCloudSDK",
   platforms: [
-    .iOS(.v17)
+    .iOS(.v15)
   ],
   products: [
     .library(
       name: "BanubaARCloudSDK",
       targets: ["BanubaARCloudSDK"]
     )
+  ],
+  dependencies: [
+    .package(url: "https://github.com/Banuba/BanubaUtilities-iOS.git", utilsVersionRange),
   ],
   targets: [
     .binaryTarget(
