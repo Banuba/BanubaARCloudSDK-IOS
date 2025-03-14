96ff024f04247848ed8d5339e525fee15aa46d5d
CI <ci@banuba.com>
2025-03-14 13:38:54 +0300

    automatic framework update

--- Package.swift
+++ Package.swift
@@ -2,21 +2,41 @@
 
 import PackageDescription
 
+let version: Version = "1.40.5"
+
 let package = Package(
   name: "BanubaARCloudSDK",
   platforms: [
-    .iOS(.v17)
+    .iOS(.v15)
   ],
   products: [
     .library(
       name: "BanubaARCloudSDK",
-      targets: ["BanubaARCloudSDK"]
+      targets: ["BanubaARCloudSDKTarget"]
     )
+  ],
+  dependencies: [
+    .package(url: "https://github.com/Banuba/BanubaUtilities-iOS.git", exact: version),
   ],
   targets: [
     .binaryTarget(
       name: "BanubaARCloudSDK",
       path: "BanubaARCloudSDK.xcframework"
+    ),
+    .target(
+      name: "BanubaARCloudSDKTarget",
+      dependencies: [
+        .target(name: "BanubaARCloudSDKWrapper")
+      ],
+      path: "BanubaARCloudSDKTarget"
+    ),
+    .target(
+      name: "BanubaARCloudSDKWrapper",
+      dependencies: [
+        "BanubaARCloudSDK",
+        .product(name: "BanubaUtilities", package: "BanubaUtilities-iOS")
+      ],
+      path: "BanubaARCloudSDKWrapper"
     )
   ]
 )
